dfb1a8ac444c7787d31b07795f0ed107f2f5b0f3
Kevin-Illu <kevinillu97@gmail.com>
2023-11-18 15:22:14 -0600

    feat(design): change the design of a all pages

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -5,6 +5,7 @@ import { type GetStaticProps, type InferGetStaticPropsType } from "next";
 import { useMDXComponent } from "next-contentlayer/hooks";
 import Head from "next/head";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const getStaticPaths = () => {
   return {
@@ -40,7 +41,7 @@ export default function SinglePostPage({
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Layout.Header withNavbar className="container post__header">
+      <Layout.Header withNavbar className="slug-container post__header">
         <div className="post__header__content">
           <h1 className="post__title">{post.title}</h1>
           <p className="post__description">{post.description}</p>
@@ -53,13 +54,15 @@ export default function SinglePostPage({
             </div>
           </div>
         </div>
-        <div>
+        <motion.div
+          whileHover={{
+            borderRadius: "36px",
+          }}
+          className="post__header__img"
+        >
           <Image src="/bg-primary.jpg" alt="hola" width={600} height={600} />
-        </div>
+        </motion.div>
       </Layout.Header>
-      <div className="slug-container">
-        <hr />
-      </div>
       <Layout.Content className="slug-container">
         <article className="post-content">
           <MDXContent />
